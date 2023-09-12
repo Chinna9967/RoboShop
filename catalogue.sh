c5760d75a2b3230c17da92fb9729a6fd7d10d65d
kashireddy <kashireddynaani9967@gmail.com>
2023-09-12 11:17:40 +0530

    Roboshop files

--- catalogue.sh
+++ catalogue.sh
@@ -1,85 +1,82 @@
 #!/bin/bash
+
 DATE=$(date +%F)
 LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-N="\e[om"
+N="\e[0m"
 Y="\e[33m"
 
-
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ];
 then
-    echo -e  " $R ERROR: please run this with root access $N"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ];
     then
-        echo -e "$2 ...$R Failure $N"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ...$G Success $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
 
-VALIDATE $? "Setting up of NPM Source"
+VALIDATE $? "Setting up NPM Source"
 
 yum install nodejs -y &>>$LOGFILE
 
-VALIDATE $? "Installing Nodejs"
+VALIDATE $? "Installing NodeJS"
 
-# once the user is created, if you run this script for the second time
-# this cmd will fail
-# improvement: first check user is already exist or not , if not exist then create
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
 useradd roboshop &>>$LOGFILE
 
-# VALIDATE $? "Setting up of NPM Source"
-
-# write a condition to check directory is existed or not
+#write a condition to check directory already exist or not
 mkdir /app &>>$LOGFILE
 
-# VALIDATE $? "Setting up of NPM Source"
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>LOGFILE
-
-VALIDATE $? "Downloading catalogue artifact"
+VALIDATE $? "downloading catalogue artifact"
 
 cd /app &>>$LOGFILE
 
-VALIDATE $? "Moving into app direcctory"
+VALIDATE $? "Moving into app directory"
 
 unzip /tmp/catalogue.zip &>>$LOGFILE
 
-VALIDATE $? "Unzipiing catalogue"
+VALIDATE $? "unzipping catalogue"
 
 npm install &>>$LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
-# give full path of catalogue.service bcoz we are inside /app
-cp E:\Devops_Shiva\Github\repos\RoboShop\Catalogue.service /etc/systemd/system/catalogue.service &>>LOGFILE
+# give full path of catalogue.service because we are inside /app
+cp E:\Devops_Shiva\Github\repos\RoboShop\Catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
-VALIDATE $? "Copying catalogue.service"
+VALIDATE $? "copying catalogue.service"
 
 systemctl daemon-reload &>>$LOGFILE
 
-VALIDATE $? "deamon reload"
+VALIDATE $? "daemon reload"
 
 systemctl enable catalogue &>>$LOGFILE
 
-VALIDATE $? "Enabling catalogue"
+VALIDATE $? "Enabling Catalogue"
 
 systemctl start catalogue &>>$LOGFILE
 
-VALIDATE $? "Starting catalogur"
+VALIDATE $? "Starting Catalogue"
 
-cp E:\Devops_Shiva\Github\repos\RoboShop\mongo.repo /etc/yum.repos.d/mongo.repo &>>LOGFILE
+cp E:\Devops_Shiva\Github\repos\RoboShop\mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "Copying mongo repo"
 
@@ -87,6 +84,6 @@ yum install mongodb-org-shell -y &>>$LOGFILE
 
 VALIDATE $? "Installing mongo client"
 
-mongo --host mongodb.kpdigital.online </app/schema/catalogue.js
+mongo --host mongodb.joindevops.online </app/schema/catalogue.js &>>$LOGFILE
 
-VALIDATE $? "Loading catalague mongodb"
+VALIDATE $? "loading catalogue data into mongodb"

bf94fb423de1a5f79f48e880560006b461029645
kashireddy <kashireddynaani9967@gmail.com>
2023-09-12 11:37:45 +0530

    Roboshop files

--- catalogue.sh
+++ catalogue.sh
@@ -60,7 +60,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # give full path of catalogue.service because we are inside /app
-cp E:\Devops_Shiva\Github\repos\RoboShop\Catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp Catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
